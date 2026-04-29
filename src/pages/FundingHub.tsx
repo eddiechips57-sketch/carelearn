e491bc8c3958ec78805d649063317671dcf72ac0
eddiechips57-sketch <eddiechips57@gmail.com>
2026-04-29 19:36:49 +0100

    Update Pexels photo: Smiling nurse

--- src/pages/FundingHub.tsx
+++ src/pages/FundingHub.tsx
@@ -76,7 +76,7 @@ export default function FundingHub() {
               <img
                 alt=""
                 className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/5214935/pexels-photo-5214935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                src="https://images.pexels.com/photos/3985163/pexels-photo-3985163.jpeg?auto=compress&cs=tinysrgb&w=1600"
               />
             </div>
             <div className="relative z-10 max-w-2xl">
